eed41b535278278ed3c69167d6a71a22b6baacdf
Jeroen Verhoeven <jve@bct.nl>
2023-06-12 16:17:23 +0200

    Fixed the icons

--- lua/custom/plugins/filetree.lua
+++ lua/custom/plugins/filetree.lua
@@ -10,6 +10,47 @@ return {
     -- Unless you are still migrating, remove the deprecated commands from v1.x
     vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
 
-    require('neo-tree').setup {}
+    require("neo-tree").setup({
+  default_component_configs = {
+    icon = {
+      folder_empty = "󰜌",
+      folder_empty_open = "󰜌",
+    },
+    git_status = {
+      symbols = {
+        renamed   = "󰁕",
+        unstaged  = "󰄱",
+      },
+    },
+  },
+  document_symbols = {
+    kinds = {
+      File = { icon = "󰈙", hl = "Tag" },
+      Namespace = { icon = "󰌗", hl = "Include" },
+      Package = { icon = "󰏖", hl = "Label" },
+      Class = { icon = "󰌗", hl = "Include" },
+      Property = { icon = "󰆧", hl = "@property" },
+      Enum = { icon = "󰒻", hl = "@number" },
+      Function = { icon = "󰊕", hl = "Function" },
+      String = { icon = "󰀬", hl = "String" },
+      Number = { icon = "󰎠", hl = "Number" },
+      Array = { icon = "󰅪", hl = "Type" },
+      Object = { icon = "󰅩", hl = "Type" },
+      Key = { icon = "󰌋", hl = "" },
+      Struct = { icon = "󰌗", hl = "Type" },
+      Operator = { icon = "󰆕", hl = "Operator" },
+      TypeParameter = { icon = "󰊄", hl = "Type" },
+      StaticMethod = { icon = '󰠄 ', hl = 'Function' },
+    }
+  },
+  -- Add this section only if you've configured source selector.
+  source_selector = {
+    sources = {
+      { source = "filesystem", display_name = " 󰉓 Files " },
+      { source = "git_status", display_name = " 󰊢 Git " },
+    },
+  },
+  -- Other options ...
+})
   end,
 }
